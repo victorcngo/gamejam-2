ff6e29b37ccacc000214db04a45582a399b37552
Typhaine Le Gales <typhaineLeGales@protontmail.com>
2024-10-17 10:17:41 +0200

    refacto: game object

--- src/js/Objects/Game.js
+++ src/js/Objects/Game.js
@@ -1,5 +1,90 @@
+import * as PIXI from 'pixi.js'
+import {radius,  hitZone} from '../settings.js'
+import Hit from './Hit.js'
+import Hold from './Hold.js'
+
 export default class Game {
-    constructor (){ 
-        
+    constructor (app){ 
+        this.hasStarted = false;
+        this.isDone = false;
+        this.playersHaveLost = false; 
+        this.targetsContainer = new PIXI.Container();
+        this.targets = []
+        this.app = app
+        this.userIsHolding = false;
+    }
+
+    init() {
+        this.setStaticObjects()
+        this.createTargets()
+        this.app.stage.addChild(this.targetsContainer);
+    }
+
+    setStaticObjects() {
+        // Static line
+        const line = new PIXI.Graphics();
+        line.lineStyle(2, 0x000000)  // Line style (black)
+            .moveTo(0, 200)          // Start point
+            .lineTo(this.app.screen.width, 200); // End point (full screen width)
+        this.app.stage.addChild(line);
+        // Static ellipse at the hit zone
+        const hitZoneCircle = new PIXI.Graphics();
+        hitZoneCircle.lineStyle(2, 0x000000)   // Outline color (black)
+            .beginFill(0xffffff)               // Fill color (white)
+            .drawCircle(hitZone, 200, radius / 2) // Circle at (375, 200) with half-radius
+            .endFill();
+        this.app.stage.addChild(hitZoneCircle);
+
+    }
+
+    createTargets () {
+        let prevX = radius;
+        let type;
+        for (let i = 0; i < 5; i++) {
+        type = Math.random() < 0.5 ? 1 : 0;
+        const length = Math.random() * (100) + 100;
+        const initXPos = Math.random() * (prevX + 100 ) + prevX;
+        if(type === 1) {
+                this.targets[i] = new Hold(length,this.targetsContainer, 'left', i, initXPos);
+                prevX = -(radius + this.targets[i].rectLength - prevX);
+            } else {
+            this.targets[i] = new Hit(this.targetsContainer, 'left', i, initXPos);
+            prevX = -(radius - prevX);
+
+            }
+        }
+    }
+
+    update() {
+        for (let i = 0; i < this.targets.length; i++) {
+            if (this.targets[i].circlePos > hitZone + 5) {
+                this.targets[i].remove(); // Remove the this.targets from the rendering
+                this.targets.splice(i, 1);  // Remove this.targets that passed hitZone
+            }
+    
+            if (!this.targets[i]) return;
+    
+            if(this.targets[i].type === 'hold') {
+                this.targets[i].moveBar()
+                if (!this.userIsHolding || i !== 0) {
+                    this.targets[i].move();
+                }
+            } else if( this.targets[i].type === 'hit') {
+                this.targets[i].move()
+            }
+        }
+        if (this.userIsHolding && this.targets[0].type === 'hold') {
+            this.targets[0].updateTimer();
+            if(this.targets[0].timeIsUp()) {
+                this.targets[0].remove() 
+                this.targets.splice(0, 1)
+                this.userIsHolding = false
+            }
+        }
+
+    }
+
+    checkResults () {
+
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,135 +1,58 @@
 import * as PIXI from 'pixi.js'
-import Hit from './Objects/Hit.js'
-import Hold from './Objects/Hold.js'
-import {radius, precision, hitZone} from './settings.js'
+
+import { precision} from './settings.js'
 import {setUpButtons, player1} from './BorneManager/borneManager.js'
+import Game from './Objects/Game.js'
 
 const createApp = async() => {
     // Create a new PixiJS application
-const app = new PIXI.Application({
-    width: window.innerWidth,   // Set canvas width
-    height: window.innerHeight,  // Set canvas height
-    resolution: window.devicePixelRatio || 1, // Set resolution to match device pixel ratio
-    antialias: true, // Enable antialiasing for smoother graphics
-    transparent: true
-  });
-  document.body.appendChild(app.view); // Append canvas to the document
+    const app = new PIXI.Application({
+        width: window.innerWidth,   // Set canvas width
+        height: window.innerHeight,  // Set canvas height
+        resolution: window.devicePixelRatio || 1, // Set resolution to match device pixel ratio
+        antialias: true, // Enable antialiasing for smoother graphics
+        transparent: true
+    });
+    document.body.appendChild(app.view); // Append canvas to the document
 
-  await setUpButtons()
+    await setUpButtons()
+    const game = new Game(app)
+    game.init()
 
   
-  // Variables
-  let choux = [];
-  let directions = ["left", "right", "up", "down"];
-  let userIsHolding = false;
-  
-  
-  // Static line
-  const line = new PIXI.Graphics();
-  line.lineStyle(2, 0x000000)  // Line style (black)
-      .moveTo(0, 200)          // Start point
-      .lineTo(app.screen.width, 200); // End point (full screen width)
-  app.stage.addChild(line);
-  // Static ellipse at the hit zone
-  const hitZoneCircle = new PIXI.Graphics();
-  hitZoneCircle.lineStyle(2, 0x000000)   // Outline color (black)
-      .beginFill(0xffffff)               // Fill color (white)
-      .drawCircle(hitZone, 200, radius / 2) // Circle at (375, 200) with half-radius
-      .endFill();
-  app.stage.addChild(hitZoneCircle);
-  
-  // Create a container for all choux objects
-  const chouxContainer = new PIXI.Container();
-  app.stage.addChild(chouxContainer);
-  
-  // Create choux
-  function createChoux() {
-      let prevX = radius;
-      let type;
-      for (let i = 0; i < 5; i++) {
-        type = Math.random() < 0.5 ? 1 : 0;
-        const length = Math.random() * (100) + 100;
-        const initXPos = Math.random() * (prevX + 100 ) + prevX;
-        if(type === 1) {
-              choux[i] = new Hold(length,chouxContainer, 'left', i, initXPos);
-              prevX = -(radius + choux[i].rectLength - prevX);
-          } else {
-            choux[i] = new Hit(chouxContainer, 'left', i, initXPos);
-            prevX = -(radius - prevX);
-
-          }
-          
-      }
-  }
-
   const handleButtonADown = () => {
-    let target = choux[0];
+    let target = game.targets[0];
       target.showFeedback()
   
       if (target.isHitCorrect() && target.type === 'hold') {
-          userIsHolding = true;
+          game.userIsHolding = true;
       }
   }
 
   const handleButtonAUp = () => {
-    userIsHolding = false;
-    choux[0].showFeedback()
-    const success = choux[0].timer < precision && choux[0].timer > - precision
-    console.log("success", success, choux[0].timer)
-
+    game.userIsHolding = false;
+    game.targets[0].showFeedback()
+    const success = game.targets[0].timer < precision && game.targets[0].timer > - precision
+    console.log("success", success, game.targets[0].timer)
   }
 
   
   player1.buttons[0].addEventListener('keydown',handleButtonADown)
   player1.buttons[0].addEventListener('keyup',handleButtonAUp)
 
-
-
-  // Main update loop -> equivalzent of draw
-  function update() {
-    for (let i = 0; i < choux.length; i++) {
-        if (choux[i].circlePos > hitZone + 5) {
-            choux[i].remove(); // Remove the choux from the rendering
-            choux.splice(i, 1);  // Remove choux that passed hitZone
-        }
-
-        if (!choux[i]) return;
-
-        if(choux[i].type === 'hold') {
-            choux[i].moveBar()
-            if (!userIsHolding || i !== 0) {
-                choux[i].move();
-            }
-        } else if( choux[i].type === 'hit') {
-            choux[i].move()
-        }
-
-        // Move only if not holding or not the first Chou
-        
-        
-
+    function update() {
+        game.update()
     }
-    if (userIsHolding && choux[0].type === 'hold') {
-        choux[0].updateTimer();
-        if(choux[0].timeIsUp()) {
-            choux[0].remove() 
-            choux.splice(0, 1)
-            userIsHolding = false
-        }
-    }
-}
   
-  // equivalent of setup
-  createChoux();
-  app.ticker.maxFPS = 60
-  app.ticker.add(update);
-  
-  // Resize handler for window resize
-  window.addEventListener('resize', () => {
-      app.renderer.resize(window.innerWidth, window.innerHeight);
-      line.clear();
-      line.lineStyle(2, 0x000000).moveTo(0, 200).lineTo(window.innerWidth, 200); // Redraw line
-  });
+    app.ticker.maxFPS = 60
+    app.ticker.add(update);
+
+    // Resize handler for window resize
+    window.addEventListener('resize', () => {
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+        line.clear();
+        line.lineStyle(2, 0x000000).moveTo(0, 200).lineTo(window.innerWidth, 200); // Redraw line
+    });
 
 }
 
