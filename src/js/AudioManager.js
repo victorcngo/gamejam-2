643b4da452c1df3acdb84aaed359bc2d41797d14
7estelle <layestelle7@gmail.com>
2024-10-18 11:08:27 +0200

    fix: audio playing multiple times

--- src/js/AudioManager.js
+++ src/js/AudioManager.js
@@ -1,5 +1,5 @@
 import { smallFarts, longFarts } from "./settings";
-import{ debounce} from './utils/debounce.js'
+import { debounce } from './utils/debounce.js'
 
 export class AudioManager {
     constructor() {
@@ -8,6 +8,7 @@ export class AudioManager {
         this.preloadSounds(smallFarts)
         this.preloadSounds(longFarts)
         this.debouncedPlay = debounce(this.play.bind(this), 1000);
+        this.activeSounds = [];
     }
 
     loadSound(name, src) {
@@ -24,19 +25,28 @@ export class AudioManager {
             this.loaded[sound.name] = true;
         });
     }
-    
+
     play(name) {
         console.log(name, this.sounds[name]);
         if (this.sounds[name]) {
             this.sounds[name].currentTime = 0;
             this.sounds[name].play();
+            this.activeSounds.push(this.sounds[name]);
         }
     }
 
-    stop(name) {
-        if (this.sounds[name]) {
-            this.sounds[name].pause();
-            this.sounds[name].currentTime = 0;
+
+    stop() {
+        this.activeSounds.forEach(audio => {
+            audio.pause();
+            audio.currentTime = 0;
+        });
+        this.activeSounds = [];
+    }
+
+    clearDebouncedPlay() {
+        if (this.debouncedPlay.cancel) {
+            this.debouncedPlay.cancel();
         }
     }
 }
